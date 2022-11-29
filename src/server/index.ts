7b05a179ceea91091c6c1d99e6a2a238e73aef89
Rafael Garcia <rafuk89@gmail.com>
2022-11-29 13:07:31 +0100

    moved file reading logic outside of endpoints. Created basic enpoints for filtering data based on categories and country

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from 'express';
-import { parse } from 'csv-parse';
+import { parse } from 'csv-parse/sync';
 import * as fs from 'fs';
 import * as path from "path";
 
 
 type Influencer = {
   influencerName: string,
-  IGName: string,
+  iGName: string,
   category1: string,
   category2: string,
   followers: string,
@@ -15,36 +15,59 @@ type Influencer = {
   engagementAvg: string
 };
 
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 const CSV_STARTING_LINE = 2; // Used to skip headers' line
+const DATA_COLUMNS = [
+  'influencerName',
+  'iGName',
+  'category1',
+  'category2',
+  'followers',
+  'audienceCountry',
+  'authEngagement',
+  'engagementAvg'
+];
 
-app.get('/', (req: Request, res: Response) => {
+let influencersData: Influencer[];
+
+try {
   const csvPath = path.resolve(__dirname, '../../data/instagram_influencers.csv');
   const fileContent = fs.readFileSync(csvPath, { encoding: 'utf-8' });
-  const columns = [
-    'Name',
-    'Instagram Name',
-    'Category 1',
-    'Category 2',
-    'Followers #',
-    'Audience Country (mostly)',
-    'Authentic engagement',
-    'Engagement average'
-  ];
 
-  parse(fileContent, {
+  influencersData = parse(fileContent, {
     delimiter: ',',
-    columns,
+    columns: DATA_COLUMNS,
     from_line: CSV_STARTING_LINE
-  }, (err, data: Influencer[]) => {
-    if (err) {
-      res.send(err);
-    }
+  });
+} catch (err) {
+  console.error(err);
+  process.exit();
+}
 
-    res.send(data);
-  })
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(influencersData);
+});
+
+app.get('/category/:catName', (req: Request, res: Response) => {
+  const { catName } = req.params;
+
+  const influencers = influencersData.filter(inf => inf.category1 === catName || inf.category2 === catName);
+  
+  res.send(influencers);
+  
+});
+
+app.get('/country/:countryName', (req: Request, res: Response) => {
+  const { countryName } = req.params;
+
+  const influencers = influencersData.filter(inf => inf.audienceCountry === countryName);
+  
+  res.send(influencers);
+  
 });
 
 app.listen(port, () => {
